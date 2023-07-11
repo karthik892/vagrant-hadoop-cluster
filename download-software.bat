1f975f75e8d9eddcb96df8a6b74abfb663be1036
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-07-11 15:10:12 -0400

    Moved software directory to subfolder

--- download-software.bat
+++ download-software.bat
@@ -1,2 +1,2 @@
 
-curl "https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-aarch64.tar.gz" -o software/hadoop-3.3.6-aarch64.tar.gz
+curl "https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-aarch64.tar.gz" -o resources/software/hadoop-3.3.6-aarch64.tar.gz
