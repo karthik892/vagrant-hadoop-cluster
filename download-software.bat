fe0053efcbb1a2190414d23f1c37a84ad1837c63
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-07-12 16:24:25 -0400

    Switched from Java 8 to Java 11

--- download-software.bat
+++ download-software.bat
@@ -1,2 +1,3 @@
 
 curl "https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-aarch64.tar.gz" -o resources/software/hadoop-3.3.6-aarch64.tar.gz
+curl "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz" -o resources/software/openjdk-11.0.1_linux-x64_bin.tar.gz
