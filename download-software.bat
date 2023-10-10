643a0b59935625c99e782ddfbf55bb4702e1cc3d
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-10-10 15:22:49 -0400

    Updated Apache Spark version

--- download-software.bat
+++ download-software.bat
@@ -1,4 +1,4 @@
 
 curl "https://dlcdn.apache.org/hadoop/common/hadoop-3.3.6/hadoop-3.3.6-aarch64.tar.gz" -o resources/software/hadoop-3.3.6-aarch64.tar.gz
 curl "https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz" -o resources/software/openjdk-11.0.1_linux-x64_bin.tar.gz
-curl "https://dlcdn.apache.org/spark/spark-3.4.1/spark-3.4.1-bin-without-hadoop.tgz" -o resources/software/spark-3.4.1-bin-without-hadoop.tgz
+curl "https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz" -o resources/software/spark-3.5.0-bin-hadoop.tgz
