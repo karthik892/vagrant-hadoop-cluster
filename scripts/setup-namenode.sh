8d480f0de3d22888866a0dfece9bf72af290b75b
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-11-10 13:38:21 -0500

    Still working on anaconda scripts

--- scripts/setup-namenode.sh
+++ scripts/setup-namenode.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "ANACONDA-PYSPARK: Generating startup scripts"
+echo source ~/anaconda3/bin/activate >> /etc/profile.d/anaconda.sh
+
 echo "NAMENODE SETUP: Setting up known hosts"
 ssh-keyscan -H node2 >> ~/.ssh/known_hosts
 
@@ -25,6 +28,14 @@ start-thriftserver.sh --hiveconf hive.server2.thrift.port=10000
 
 echo "NAMENODE HBASE: Starting HBase"
 $HBASE_HOME/bin/start-hbase.sh
+
+# echo "ANACONDA-PYSPARK: Generating Startup scripts"
+# sudo echo export ANACONDA_HOME=~/anaconda3 >> /etc/profile.d/anaconda.sh
+# sudo echo source $ANACONDA_HOME/bin/activate >> /etc/profile.d/anaconda.sh
+# sudo echo conda activate hadoop >> /etc/profile.d/anaconda.sh
+
+#echo "ANACONDA-PYSPARK: Setting Jupyter config"
+#cp /vagrant/resources/jupyter ~/.jupyter/jupyter_notebook_config.py
 
 echo "Dumping startup logs"
 /vagrant/bin/dumplogs.sh
