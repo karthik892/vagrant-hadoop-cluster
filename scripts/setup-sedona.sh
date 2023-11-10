14617bc5738231de865619700d8b49f9e2f5e92d
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-11-10 13:37:24 -0500

    Stable sedona install

--- scripts/setup-sedona.sh
+++ scripts/setup-sedona.sh
@@ -1,6 +1,4 @@
-apt-get install -y python3-pip
-pip install apache-sedona
-cp /vagrant/resources/software/apache-sedona-1.2.1-incubating-bin/sedona-python-adapter-3.0_2.12-1.2.1-incubating.jar $SPARK_HOME/jars
-cp /vagrant/resources/software/apache-sedona-1.2.1-incubating-bin/sedona-core-3.0_2.12-1.2.1-incubating.jar $SPARK_HOME/jars
-cp /vagrant/resources/software/apache-sedona-1.2.1-incubating-bin/sedona-viz-3.0_2.12-1.2.1-incubating.jar $SPARK_HOME/jars
-cp /vagrant/resources/software/geotools-wrapper-1.1.0-25.2.jar $SPARK_HOME/jars
+#!/bin/bash
+
+cp /vagrant/resources/software/geotools-wrapper-1.5.0-28.2.jar $SPARK_HOME/jars
+cp /vagrant/resources/software/apache-sedona-1.5.0-bin/sedona-spark-shaded-3.0_2.12-1.5.0.jar $SPARK_HOME/jars
