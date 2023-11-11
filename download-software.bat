714aa91cf3d883afff21464e40c4056bc5ede40c
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-11-11 18:34:37 -0500

    BUGFIX: Incorrect file name

--- download-software.bat
+++ download-software.bat
@@ -5,7 +5,7 @@ curl "https://dlcdn.apache.org/spark/spark-3.4.1/spark-3.4.1-bin-hadoop3.tgz" -o
 curl "https://dlcdn.apache.org/spark/spark-3.5.0/spark-3.5.0-bin-hadoop3.tgz" -o resources/software/spark-3.5.0-bin-hadoop.tgz
 curl "https://dlcdn.apache.org/hive/hive-3.1.3/apache-hive-3.1.3-bin.tar.gz" -o resources/software/apache-hive-3.1.3-bin.tar.gz
 REM TODO Update sedona
-curl "https://dlcdn.apache.org/sedona/1.5.0/apache-sedona-1.5.0-bin.tar.gz" -o resources/software/apache-sedona-1.4.1-bin.tar.gz
+curl "https://dlcdn.apache.org/sedona/1.5.0/apache-sedona-1.5.0-bin.tar.gz" -o resources/software/apache-sedona-1.5.0-bin.tar.gz
 curl "https://repo1.maven.org/maven2/org/datasyslab/geotools-wrapper/1.5.0-28.2/geotools-wrapper-1.5.0-28.2.jar" -o resources/software/geotools-wrapper-1.5.0-28.2.jar
 curl "https://dlcdn.apache.org/zookeeper/zookeeper-3.8.3/apache-zookeeper-3.8.3-bin.tar.gz" -o resources/software/apache-zookeeper-3.8.3-bin.tar.gz
 curl "https://dlcdn.apache.org/hbase/2.5.5/hbase-2.5.5-bin.tar.gz" -o resources/software/hbase-2.5.5-bin.tar.gz
