4be5feb4c5118e3701e1f107a51d926864ec0bb4
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2023-11-11 18:37:05 -0500

    Automatically extract tar.gz file

--- scripts/setup-sedona.sh
+++ scripts/setup-sedona.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+tar -xzf /vagrant/resources/software/apache-sedona-1.5.0-bin.tar.gz -C /usr/local
 
 cp /vagrant/resources/software/geotools-wrapper-1.5.0-28.2.jar $SPARK_HOME/jars
-cp /vagrant/resources/software/apache-sedona-1.5.0-bin/sedona-spark-shaded-3.0_2.12-1.5.0.jar $SPARK_HOME/jars
+cp /usr/local/apache-sedona-1.5.0-bin/sedona-spark-shaded-3.0_2.12-1.5.0.jar $SPARK_HOME/jars
