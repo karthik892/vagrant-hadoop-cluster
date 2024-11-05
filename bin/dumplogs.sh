a6070784d88b1e63ab7e611b7fafa8ebb661bc3c
Karthik Sankaran <Karthik.Sankaran@njit.edu>
2024-11-05 10:20:19 -0500

    Added zookeeper logs

--- bin/dumplogs.sh
+++ bin/dumplogs.sh
@@ -11,12 +11,12 @@ cp $SPARK_HOME/logs/* /vagrant/logs/spark
 journalctl -u spark.service >> /vagrant/logs/spark/systemd.log
 journalctl -u spark-history-server.service >> /vagrant/logs/spark/systemd-historyserver.log
 
-#mkdir /vagrant/logs/zk
-#cp $ZK_HOME/logs/* /vagrant/logs/zk
-#journalctl -u zookeeper.service >> /vagrant/logs/zookeeper/systemd.log
+mkdir /vagrant/logs/zk
+cp $ZK_HOME/logs/* /vagrant/logs/zk
+journalctl -u zookeeper.service >> /vagrant/logs/zk/systemd.log
 
-mkdir /vagrant/logs/hive
-journalctl -u hive.service >> /vagrant/logs/hive/systemd.log
+#mkdir /vagrant/logs/hive
+#journalctl -u hive.service >> /vagrant/logs/hive/systemd.log
 
 # mkdir /vagrant/logs/hbase
 # cp $HBASE_HOME/logs/* /vagrant/logs/hbase
